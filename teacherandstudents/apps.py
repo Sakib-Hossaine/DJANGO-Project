742d0d62ededc0984803eb57a26519d11d41946c
Your Name <you@example.com>
2025-04-25 16:28:20 +0600

    name change

--- teacherandstudents/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TeacherandstudentsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'teacherandstudents'
--- teacherandstudents/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('profiles/', views.profile, name='profile'),
-    path('profiles/marks/', views.student_marks, name='student_marks'),
-]
